e8b8fcfb0bbf7ca45e3bece40c2dcb83979679c9
aW3ikun_Work <z1002280879@gmail.com>
2021-06-22 17:18:19 +0800

    增加卸载功能，选择dll 完善功能

--- C++/Inject/MFC远程线程注入/demo/resource.h
+++ C++/Inject/MFC远程线程注入/demo/resource.h
@@ -9,6 +9,10 @@
 #define IDR_MAINFRAME                   128
 #define IDC_EDIT_ProcessId              1000
 #define IDC_BUTTON_Inject               1001
+#define IDC_EDIT_DllPath                1002
+#define IDC_BUTTON1                     1004
+#define IDC_BUTTON_Dll                  1004
+#define IDC_BUTTON_UnLoad               1005
 
 // Next default values for new objects
 // 
@@ -16,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
