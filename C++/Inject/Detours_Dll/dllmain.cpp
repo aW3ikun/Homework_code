ec3be4bed7daad2379a6d87bb39abc4c68c62f9a
aW3ikun <z1002280879@gmail.com>
2021-06-24 18:39:56 +0800

    使用Pipe进行数据传输

---
+++ C++/Inject/Detours_Dll/dllmain.cpp
@@ -0,0 +1,28 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include<Windows.h>
+#include<iostream>
+#include"detours/detours.h"
+#pragma comment(lib,"detours\\detourslib_X86\\detours.lib")
+
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH: {
+        ;
+    }
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH: {
+        ;
+    }
+        break;
+    }
+    return TRUE;
+}
+
