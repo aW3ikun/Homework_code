4e3be306600028319cbd65a88557f624022e5a06
aW3ikun <socsgogo1@gmail.com>
2021-06-30 03:00:39 +0800

    DetoursHook WriteFile 4位信息过滤

--- C++/Inject/Detours_Dll/help.h
+++ C++/Inject/Detours_Dll/help.h
@@ -1,21 +1,31 @@
 #pragma once
-#include"global.h"
+#include<Windows.h>
+#include<process.h>
+#include"detours/detours.h"
 
+#pragma comment(lib,"detours/detourslib_X86/detours.lib")
+#define BUFSIZE 512
 
-class DetoursMyHook
-{
+#pragma comment(linker, "/INCLUDE:__tls_used")
+static __declspec(thread) TCHAR lpMessage[BUFSIZE] = { 0 };
 
-public:
-	DetoursMyHook(map<LPVOID, LPVOID>, BOOL );
-	DetoursMyHook();
-	~DetoursMyHook();
-	void InsertMap(map<LPVOID, LPVOID>,BOOL );
-private:
-	VOID SetHook();
-	VOID UnHook();
-	map<LPVOID, LPVOID> mapHookFunc;
-	BOOL bHook;
+typedef BOOL(WINAPI* pfnWriteFile)(
+	HANDLE       hFile,
+	LPCVOID      lpBuffer,
+	DWORD        nNumberOfBytesToWrite,
+	LPDWORD      lpNumberOfBytesWritten,
+	LPOVERLAPPED lpOverlapped
+	);
 
-};
+unsigned int WINAPI ThreadProc(LPVOID pParam);
+BOOL Log(LPWSTR lpszStr);
 
+BOOL WINAPI  NewWriteFile(HANDLE       hFile,
+	LPCVOID      lpBuffer,
+	DWORD        nNumberOfBytesToWrite,
+	LPDWORD      lpNumberOfBytesWritten,
+	LPOVERLAPPED lpOverlapped);
 
+static BOOL bHook = FALSE;
+VOID SetHook();
+VOID UnHook();
