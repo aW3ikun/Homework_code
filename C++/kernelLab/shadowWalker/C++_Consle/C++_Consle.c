e55075c514cefbb52ef100fc4921a256b89ff99c
aW3ikun <z1002280879@gmail.com>
2021-07-02 18:37:28 +0800

    写到一半

--- C++/kernelLab/shadowWalker/C++_Consle/C++_Consle.c
+++ C++/kernelLab/shadowWalker/C++_Consle/C++_Consle.c
@@ -2,10 +2,19 @@
 #include<stdlib.h>
 #include<windows.h>
 
+#define PTE(x)	((DWORD *)(0xC0000000 + ((x >> 12) << 3)))  
+#define PDE(x)	((DWORD *)(0xC0060000 + ((x >> 21) << 3)))
+
 #define K_ESP 0x8003f3f0 //error_code
 #define K_ESP_4 0x8003f3f4 //eip
 #define K_TARGET_CR3 0x8003f3e0 
 #define K_CR2 0x8003f3e4
+
+#define K_REAL_PTE0	0x8003f3d0 
+#define K_REAL_PTE1 0x8003f3d4 
+#define K_FAKE_PTE0 0x8003f3c0 
+#define K_FAKE_PTE1 0x8003f3c4 
+
 
 //KiTrap0E
 // error_code
@@ -20,12 +29,21 @@
 DWORD g_esp;
 DWORD g_esp_4;
 DWORD g_cr2; //产生页保护的虚拟地址
+
+#pragma section("mydata",read,write) 
+__declspec(allocate("mydata")) DWORD FakePage[1024]; //0x41c000
+       
 //0x0401080
 //__declspec(naked)  直接作为汇编使用
 void __declspec(naked) IdtEntry1()
 {
-	__asm {
+	*(DWORD*)K_REAL_PTE0 = PTE(0x412000)[0];
+	*(DWORD*)K_REAL_PTE1 = PTE(0x412000)[1];
+	*(DWORD*)K_FAKE_PTE0 = PTE(0x412000)[0];
+	*(DWORD*)K_FAKE_PTE1 = PTE(0x412000)[1];
 
+
+	__asm {
 		mov eax, cr3
 		mov ds : [K_TARGET_CR3] , eax
 
@@ -54,6 +72,7 @@ void go() {
 }
 //eq 8003f500 0040ee00`00081080
 void  main() {
+	FakePage[0] = 0;
 	if ((DWORD)IdtEntry1 != 0x0401080) {
 		printf("wrong addr：%p\n", IdtEntry1);
 		system("pause");
