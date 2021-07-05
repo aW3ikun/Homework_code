b68c1d2b793eb519873218beb9bfe6ae9d9c7e7a
aW3ikun <socsgogo1@gmail.com>
2021-07-05 19:17:45 +0800

    试过使用SetConsoleCtrlHandler 进行退出pte恢复，但未知异常。Shadowwalker完成课程。

--- C++/kernelLab/shadowWalker/CopyFunToKer/CopyFunToKer.cpp
+++ C++/kernelLab/shadowWalker/CopyFunToKer/CopyFunToKer.cpp
@@ -10,6 +10,11 @@
 #define K_TARGET_CR3 0x8003f3e0
 #define K_CR2 0x8003f3e4
 
+#define K_REAL_PTE0	0x8003f3d0 
+#define K_REAL_PTE1 0x8003f3d4 
+#define K_FAKE_PTE0 0x8003f3d8
+#define K_FAKE_PTE1 0x8003f3dc
+
 
 //target 0x8003f120
 //0x8053e545
@@ -28,6 +33,8 @@ void __declspec(naked) IdtEntry1()
 		*p = ((char*)JmpTarget)[i];
 		p++;
 	}
+	p = (char*)0x8003f230;
+
 	__asm {
 		mov eax,0xffffffff
 		mov ds:[K_TARGET_CR3],eax
@@ -55,24 +62,59 @@ void __declspec(naked) IdtEntry1()
 }
 void __declspec(naked) JmpTarget() {
 	__asm {
-		push eax
+		pushad
 		mov eax, cr3
 		cmp eax, ds: [K_TARGET_CR3]
 		jnz PASS
 
 		//0x412000
-		mov eax,cr2
-		shr eax,0xc
-		cmp eax,0x412
+		mov eax, cr2
+		shr eax, 0xc
+		cmp eax, 0x412
 		jnz PASS
 
+		mov eax, ss: [esp + 0x20]
+		test eax,0x10
+		jnz  EXECUTE
+		jmp READ_WRITE
+EXECUTE :
+	}
+		//执行挂上正确的页面 然后放到tlb中，然后恢复
+	PTE(0x412000)[0] = *(DWORD*)K_REAL_PTE0;
+	PTE(0x412000)[1] = *(DWORD*)K_REAL_PTE1;
 
+	__asm {
+		mov eax, 0x0412018
+		call eax
+	}
+	PTE(0x412000)[0] = 0;
+		PTE(0x412000)[1] = 0x0;
+	__asm {
+		popad
+		add esp, 0x4
+		iretd
 
-PASS :
-		pop eax
-			mov     word ptr[esp + 2], 0
-			push 0x80541457
-			ret
+READ_WRITE:
+	}
+	PTE(0x412000)[0] = *(DWORD*)K_FAKE_PTE0;
+	PTE(0x412000)[1] = *(DWORD*)K_FAKE_PTE1;
+		__asm {
+			mov eax, ds: [0x412000]
+		}
+
+		//会导致tlb失效
+		//PTE(0x412000)[0] = 0;
+		//PTE(0x412000)[1] = 0;
+		__asm {
+		popad
+		add esp,0x4
+		iretd
+
+PASS:
+		popad
+		mov     word ptr[esp + 2], 0
+		push 0x80541457
+		ret
 	}
 }
 void go() {
