10f74ef0b7aaa416788ff8773f3e639abffa94bc
aW3ikun <socsgogo1@gmail.com>
2021-07-22 00:20:16 +0800

    工厂模式开头

---
+++ C++/design_pattern/Factory/simpleFactory.cpp
@@ -0,0 +1,6 @@
+// https://www.cnblogs.com/schips/p/common-design-pattern-about-cpp.html
+#include<iostream>
+
+int main() {
+
+}
--- C++/design_pattern/Singleton/main.cpp
+++ C++/design_pattern/Singleton/main.cpp
@@ -1,7 +1,7 @@
 //https://www.cnblogs.com/schips/p/common-design-pattern-about-cpp.html
 
 #include<iostream>
-using namespace std;
+#include <mutex>
 
 //单例类只能由一个实例化对象。
 //单例类必须自己提供一个实例化对象。
@@ -11,9 +11,7 @@ class Singleton {
 public:
 	static Singleton* getInstance();
 	~Singleton() {};
-
 private:
-	static int getCount();
 	Singleton() {};
 	Singleton(const Singleton&) = delete; //明确拒赋值构造
 	Singleton& operator = (const Singleton&) = delete; //明确拒绝等号运算符重载
@@ -21,8 +19,44 @@ private:
 	static  Singleton* m_pSingleton; //静态成员
 };
 
-Singleton*  Singleton::m_pSingleton = NULL;
+
+////lazy initialization  非线程安全
+// Singleton* Singleton::m_pSingleton = NULL;
+//Singleton* Singleton::getInstance() {
+//	if (m_pSingleton == NULL) {
+//		m_pSingleton = new Singleton;
+//	}
+//	return m_pSingleton;
+//}
+
+////lazy initialization 线程安全
+// Singleton* Singleton::m_pSingleton = NULL;
+//std::mutex mt;
+//Singleton* Singleton::getInstance() {
+//	if (m_pSingleton == NULL) {
+//		mt.lock();
+//		if (m_pSingleton == NULL) {
+//			m_pSingleton = new Singleton();
+//		}
+//		mt.unlock();
+//	}
+//	return m_pSingleton;
+//}
+
+//early initialization 线程安全
+Singleton* Singleton::m_pSingleton = new Singleton();
+Singleton* Singleton::getInstance() {
+	return m_pSingleton;
+}
+
+
 
 int main() {
+	Singleton* s1 = Singleton::getInstance();
+	Singleton* s2 = Singleton::getInstance();
+	printf("%p,%p\n", s1, s2);
+
+	delete s1;
+
 	return 0;
 }
