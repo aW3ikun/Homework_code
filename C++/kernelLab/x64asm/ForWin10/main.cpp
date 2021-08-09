1e207303b5394d71956fdf96053f6b658eee0404
aW3ikun <socsgogo1@gmail.com>
2021-08-09 14:30:50 +0800

    KPTI 内核页表隔离机制验证 开启KVAS

--- C++/kernelLab/x64asm/ForWin10/main.cpp
+++ C++/kernelLab/x64asm/ForWin10/main.cpp
@@ -1,7 +1,7 @@
 #include<cstdio>
 #include<stdlib.h>
 #include<Windows.h>
-// 固定机制，关增量链接
+// 固定基地址，关增量链接
 //int 21h fffff803`4f25b210  4c9f8e00`0010c388 00000000`fffff803
 //eq fffff803`4f25b210 4000ee00`001010e0
 //eq fffff803`4f25b218 00000000`00000001
@@ -16,11 +16,13 @@ extern "C" ULONG64 x;
 ULONG64 x;
 
 int main() {
-	if ((ULONG64)IdtEntry != 0x00000001400010E0) {
+	if ((ULONG64)IdtEntry != 0x00000001400010F0) {
 		printf("wrong IdtEntry at %p\n", IdtEntry);
 		system("pause");
 		exit(-1);
 	}
+	system("pause");
+	// 修改pxe权限为可执行。
 	go();
 	printf("%p\n",x);
 	system("pause");
--- C++/kernelLab/x64asm/ForWin10/x64asm.asm
+++ C++/kernelLab/x64asm/ForWin10/x64asm.asm
@@ -9,23 +9,29 @@ extern	 x:qword
 .code
 
 IdtEntry Proc
-	;mov rax, cr3
-	;mov x,rax
-	mov     rsp, gs:7000h
-	mov     cr3, rsp
-	
-	mov     rsp, gs:7008h
-	mov     gs:10h, rsi
-	mov     rsi, gs:38h
-	add     rsi, 4200h
-	push    qword ptr [rsi-8]
-	push    qword ptr [rsi-10h]
-	push    qword ptr [rsi-18h]
-	push    qword ptr [rsi-20h]
-	push    qword ptr [rsi-28h]
-	mov     rsi, gs:10h
-	and     qword ptr gs:10h, 0
 
+	swapgs	
+	mov     gs:[7010h], rsp
+	mov     rsp, gs:[7000h]
+	bt      dword ptr gs:[7018h], 1
+	jb      short @AA
+	mov     cr3, rsp
+@AA:
+	 mov     rsp, gs:[7008h]; 获取rsp
+	 mov      gs:[10h], rsi
+	 mov     rsi, gs:[38h]
+	 add     rsi, 4200h      ; idtbase+4200h处保存中断前的信息
+	 push    qword ptr [rsi-8] ; ss
+	 push    qword ptr [rsi-10h] ; rsp
+	 push    qword ptr [rsi-18h] ; rflags
+	 push    qword ptr [rsi-20h] ; cs
+	 push    qword ptr [rsi-28h] ; rip
+	 mov     rsi, gs:[10h]
+	 and     qword ptr gs:[10h], 0 
+	sti
+
+@L:
+	jmp @L
 
 	iretq
 IdtEntry Endp
