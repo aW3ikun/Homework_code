ab89f30531aec273731ea3c1b9fc5c9de72bec92
aW3ikun <socsgogo1@gmail.com>
2021-11-12 18:47:48 +0800

    c++内存对象学习

--- C++/Inject_Hook/虚表Hook_VMTHook/Test_虚表/Main.cpp
+++ C++/Inject_Hook/虚表Hook_VMTHook/Test_虚表/Main.cpp
@@ -1,79 +1,124 @@
 #include<iostream>
 using namespace std;
 
-class VirtualBase
-{
-	//Pure	virtual	functions
+
+class NewBase {
 	public:
-	VirtualBase( )
-	{
-		x = 1;
-	}
-	virtual void Demon( ) = 0;
-	virtual	void Base( ) { cout << "this is father class" << endl; }
-	virtual void Base2( ) { cout << "this is father2 class" << endl; };
+	void Go(){ cout << "this is Base  " << endl; };
 	private:
-	int x;
+	int m_a;
 };
 
-class SubVirtual :public VirtualBase {
-	public:
-
-	SubVirtual( )
-	{
-		y = 2;
-	}
-	void Demon( ) { cout << "SubVirtual Demon" << endl; };
-	//void Base( ) { cout << "this is SubVirtual class" << endl; };
-	private:
-	int y;
-
-};
-class SubVirtual2 :public VirtualBase {
-	public:
-	SubVirtual2( )
-	{
-		z = 3;
-	}
-	void Demon( ) { cout << "SubVirtual2 Demon" << endl; };
-	void Base( ) { cout << "this is SubVirtual2 class" << endl; };
-	private:
-	int z;
-};
-
-class SubVirtual3 :public SubVirtual2 {
-	public:
-	SubVirtual3( )
-	{
-		m = 4;
-	}
-	void Base( ) { cout << "this is SubVirtual3 class" << endl; };
-	private:
-	int m;
-};
-
-void call(VirtualBase* vb)
-{
-	vb->Demon( );
-	vb->Base( );
-	vb->Base2( );
-}
-
+//class VirtualBase
+//{
+//	//Pure	virtual	functions
+//	public:
+//	VirtualBase( )
+//	{
+//		m_a = 1;
+//	}
+//	virtual void Demon( ) = 0;
+//	virtual	void Base( ) { cout << "this is VirtualBase Base()" << endl; }
+//	virtual void Base2( ) { cout << "this is VirtualBase  Base2()" << endl; };
+//	private:
+//	int m_a;
+//};
+//class VirtualBase2
+//{
+//	//Pure	virtual	functions
+//	public:
+//	VirtualBase2( )
+//	{
+//		m_b = 1;
+//	}
+//	virtual void Demon( ) = 0;
+//	virtual	void Base( ) { cout << "this is VirtualBase2 Base()" << endl; }
+//	virtual void Base2( ) { cout << "this is VirtualBase2  Base2()" << endl; };
+//	private:
+//	int  m_b;
+//};
+//
+//class SubVirtual :public VirtualBase {
+//	public:
+//
+//	SubVirtual( )
+//	{
+//		m_c = 2;
+//	}
+//	void Demon( ) { cout << "SubVirtual Demon" << endl; };
+//	//void Base( ) { cout << "this is SubVirtual class" << endl; };
+//	void Go( ) { cout << "this is SubVirtual4 GO" << endl; }
+//	private:
+//	int  m_c;
+//
+//};
+//class SubVirtual2 :public VirtualBase {
+//	public:
+//	SubVirtual2( )
+//	{
+//		m_d = 3;
+//	}
+//	void Demon( ) { cout << "SubVirtual2 Demon" << endl; };
+//	void Base( ) { cout << "this is SubVirtual2 class" << endl; };
+//	private:
+//	int  m_d;
+//};
+//
+//class SubVirtual3 :public SubVirtual2 {
+//	public:
+//	SubVirtual3( )
+//	{
+//		m_e = 4;
+//	}
+//	void Base( ) { cout << "this is SubVirtual3 class" << endl; };
+//	private:
+//	int  m_e;
+//};
+//
+//class SubVirtual4 :public VirtualBase, public VirtualBase2 {
+//	public:
+//	SubVirtual4( )
+//	{
+//		m_f = 4;
+//	}
+//	void Demon( ) { cout << "SubVirtual4 class" << endl; }
+//	void Base( ) { cout << "this is SubVirtual4 class" << endl; };
+//
+//	private:
+//	int m_f;
+//};
+//
+//void call(VirtualBase* vb)
+//{
+//	vb->Demon( );
+//	vb->Base( );
+//	vb->Base2( );
+//}
+//
 
 int main(int argc, char* argv[])
 {
+	NewBase* base = new NewBase( );
+	base->Go( );
 
-	VirtualBase* sv = new	SubVirtual( );
-	VirtualBase* sv2 = new	SubVirtual2( );
-	VirtualBase* sv3 = new	SubVirtual3( );
+	//VirtualBase* sv = new	SubVirtual( );
+	//SubVirtual* sub = new	SubVirtual( );
+	//VirtualBase* sv = sub;
+	//VirtualBase* sv2 = new	SubVirtual2( );
+	//VirtualBase* sv3 = new	SubVirtual3( );
+	//VirtualBase* sv4 = new	SubVirtual4( );
+	//sub->Go( );
 
-	call(sv);
-	cout << "======================" << endl;
-	call(sv2);
-	cout << "======================" << endl;
-	call(sv3);
 
-	type_info
+
+	//call(sv);
+	//cout << "======================" << endl;
+	//call(sv2);
+	//cout << "======================" << endl;
+	//call(sv3);
+	//cout << "======================" << endl;
+	//call(sv4);
+
 
 	system("pause");
 	return 0;
